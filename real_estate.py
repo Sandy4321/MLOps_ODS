7aa15fc07df9b76de0f82dcacf58a490b673d08a
Gmurad735 <abd.gm90@yandex.ru>
2022-08-24 20:31:37 +0300

    initial_commit

--- real_estate.py
+++ real_estate.py
@@ -16,8 +16,6 @@ MAX_PRICE = 100_000_000
 MIN_KITCHEN = 3  # Outlier range for kitchen area
 MAX_KITCHEN = 70
 
-NEEDED_PARAM = None
-
 
 def clean_data(input_path: str,) -> pd.DataFrame:
     """Function removes excess columns and enforces
